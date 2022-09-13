c142362551b7e6d67e9fce8817d37f2fd221f3ba
rudradcruze <francisrudra@gmail.com>
2022-09-13 12:10:13 +0600

    Calculator value set

--- js/app.js
+++ js/app.js
@@ -17,3 +17,25 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     const displayPin = document.getElementById('display-pin');
     displayPin.value = getPin();
 })
+
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const buttonValue = event.target.innerText;
+    const typingPing = document.getElementById('typing-pin');
+    if(isNaN(buttonValue))
+    {
+        if(buttonValue === 'C')
+            typingPing.value = '';
+        if(buttonValue === '<')
+        {
+            const removeAble = typingPing.value.split('');
+            removeAble.pop();
+            const joinPin = removeAble.join('');
+            typingPing.value = joinPin;
+        }
+    }
+    else
+    {
+        const previousValue = typingPing.value;
+        typingPing.value = previousValue + buttonValue;
+    }
+})
